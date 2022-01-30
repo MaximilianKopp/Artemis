aceeac9a0e01d1de1464ecbeba66429390a31999
mkopp <m.kopp89@web.de>
2022-01-30 16:24:54 +0100

    Create StartMenu Composition

---
+++ app/src/main/java/com/ataraxia/hunterexam/templates/TextTemplate.kt
@@ -0,0 +1,14 @@
+package com.ataraxia.hunterexam.templates
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+abstract class TextTemplate {
+    companion object {
+
+        @Composable
+        fun menuStyle() = MaterialTheme.typography.body1
+        val MENU_COLOR = Color.Black
+    }
+}
